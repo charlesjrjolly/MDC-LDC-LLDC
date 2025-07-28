b3e8134e2a5df448ae37fb4e0bf90bc473ec6dda
charlesjrjolly <136286469+charlesjrjolly@users.noreply.github.com>
2025-07-28 19:14:10 -0300

    Add files via upload

---
+++ mdc-vs-ldc/mdc-vs-ldc/README.md
@@ -0,0 +1,6 @@
+# MDC vs LDC
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/jcc882226/pen/JjgOOZj](https://codepen.io/jcc882226/pen/JjgOOZj).
+
---
+++ mdc-vs-ldc/mdc-vs-ldc/dist/script.js
@@ -0,0 +1,18 @@
+function green(){
+  var MDC = ["Norway","Australia","New Zealand","Canada ", "United States Of America", "Portugal","Slovenia","Qatar","Poland","United Kingdom of Great Britain and Northern Ireland","Sweden","Spain","Singapore","Malta","Luxembourg","Korea (the Republic of)	","Japan","Ireland","Italy","Iceland","Hong Kong","Hungary","Cyprus","Denmark","France","Finland","Greece","China","Austria","Slovenia","Bahrain","Brunei Darussalam"];
+  var why2  = MDC[(Math.floor(Math.random() * MDC.length))];
+  alert(why2);
+  
+}
+
+function yellow(){
+  var LDC = ["Chile	","Costa Rica	","Egypt","Georgia","Jordan","Malaysia"];
+ var why  = LDC[(Math.floor(Math.random() * LDC.length))];
+  alert(why);
+}
+
+function red(){
+  var LLDC = ["Senegal","Tanzania, United Republic of	","Togo","Uganda","Zambia","Yemen","Zimbabwe","Somalia","Rwanda","Nigeria","Niger","Nepal","Myanmar","Mozambique","Mali","Mauritius","Liberia","Lesotho","Guinea-Bissau","Guinea","Haiti","Kenya","Madagascar","Malawi","Ghana","Gambia","Ethiopia","Congo (the Democratic Republic of the)","Côte d'Ivoire(Ivory Coast)","Djibouti","Congo","Chad","Burundi","Burkina Faso	","Benin","Cameroon","Burundi","Bangladesh","Barbados","Afghanistan","central African Republic","Papua New Guinea	","Sierra Leone","Comoros"];
+  var why1  = LLDC[(Math.floor(Math.random() * LLDC.length))];
+  alert(why1);
+}
